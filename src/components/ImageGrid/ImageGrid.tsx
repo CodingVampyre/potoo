2c6dc9abfacef6fbdb54bec94573041d172613e2
Tobias Kavsek <tobiaskavsek@hotmail.de>
2020-08-18 23:01:14 +0200

    :wrench: add image renderer

--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import './ImageGrid.css';
 import { Storage } from '../../classes/storage.object';
 
@@ -6,13 +7,19 @@ const { ipcRenderer } = window.require('electron');
 
 export function ImageGrid() {
 
-    ipcRenderer.on('fileLoadedIntoMemory', (event, images: Uint8Array[]) => {
-        console.log('images', images);
+    let [displayImages, setDisplayImages] = useState<string[]>([]);
+
+    ipcRenderer.on('fileLoadedIntoMemory', (event, images: string[]) => {
+        setDisplayImages(images);
     });
 
     return (
         <div className={"image-grid-master"} >
             <button onClick={() => ipcRenderer.send('openImageFileDialog')}>Add Image</button>
+            { displayImages.map(displayImage => <div key={displayImage}>
+                <img src={`data:image/png;base64, ${displayImage}`} alt={'broken image'} />
+            </div>) }
+            <p>{displayImages.length}</p>
         </div>
     );
 }
