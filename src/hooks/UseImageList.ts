7f41d1861d8f6d7d65b210e6aa39dbdb767335d7
Tobias Kavsek <tobiaskavsek@hotmail.de>
2020-08-21 21:17:04 +0200

    :wrench: fix ipc rederer storing images too often

--- src/hooks/UseImageList.ts
+++ src/hooks/UseImageList.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 const { ipcRenderer } = window.require('electron');
 
@@ -18,35 +18,40 @@ export function useImageList(): IUseImageListHook {
 
     const [images, setImages] = useState<IImage[]>([]);
 
+    useEffect(() => {
+        ipcRenderer.on('fetchImagesResult', (event, images: IImage[]) => {
+            console.debug('[fetchImagesResult]', images);
+            setImages(images);
+        });
+
+        ipcRenderer.on('storeImageResult', (event) => {
+            console.debug('[storeImageResult]');
+        });
+
+        ipcRenderer.on('openImageFileDialogResult', (event, imagesBase64: string[]) => {
+            console.log('[openImageFileDialogResult]', imagesBase64);
+            const images: IImage[] = imagesBase64.map(imageBase64 => {
+                return { id: uuid(), imageBase64, tags: [], }
+            });
+            storeImages(images);
+        });
+    }, []);
+
     ipcRenderer.on('log', (event, message: string) => { console.log(message); })
 
-    ipcRenderer.on('openImageFileDialogResult', (event, imagesBase64: string[]) => {
-        console.log('[openImageFileDialogResult]', imagesBase64);
-        const images: IImage[] = imagesBase64.map(imageBase64 => {
-            return { id: uuid(), imageBase64, tags: [], }
-        });
-        storeImages(images);
-    });
-
-    ipcRenderer.on('fetchImagesResult', (event, images: IImage[]) => {
-        console.debug('[fetchImagesResult]', images);
-        setImages(images);
-    });
-
-    ipcRenderer.on('storeImageResult', (event) => {
-        console.debug('[storeImageResult]');
-    });
-
+    /** */
     function fetchImages() {
         console.log('r --> e (fetchImages)');
         ipcRenderer.send('fetchImages');
     }
 
+    /** */
     function storeImages(images: IImage[]) {
         console.log('r --> e (storeImages)');
         ipcRenderer.send('storeImages', images);
     }
 
+    /** */
     function openImageFileDialog() {
         console.log('r --> e (openImageFileDialog)');
         ipcRenderer.send('openImageFileDialog');
